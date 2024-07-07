a0d97489eff085721438ad2f2264a0c566f54c88
tepk2924 <chohan0925@gmail.com>
2024-07-07 23:23:28 +0900

    필요없는 build, devel 파일 git 저장소에에서 삭제 처리

--- ros_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tepk2924/tepk2924Works/myGraduationProject/ros_ws/src
-# Build directory: /home/tepk2924/tepk2924Works/myGraduationProject/ros_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("pkg1")
--- ros_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/tepk2924/tepk2924Works/myGraduationProject/ros_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/tepk2924/tepk2924Works/myGraduationProject/ros_ws/devel;/opt/ros/noetic'.split(';') if '/home/tepk2924/tepk2924Works/myGraduationProject/ros_ws/devel;/opt/ros/noetic' != '' else []
